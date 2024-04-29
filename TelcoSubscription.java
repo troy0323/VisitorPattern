99c3a3cc9ffab5abb6d1348df938413c95c92811
Troy <90951302+troy0323@users.noreply.github.com>
2024-04-28 22:32:11 -0500

    Update TelcoSubscription.java

--- TelcoSubscription.java
+++ TelcoSubscription.java
@@ -1,7 +1,8 @@
-interface TelcoSubscription {
-    String accept(UsagePromo usagePromo, UnliCallOffer unliCallOffer);
+public interface TelcoSubscription{
+    public String accept(UsagePromo promo, double price);
+    public String accept(UnliCallOffer offer, boolean unliCallText);
+    public String getTelcoName();
+    public double getPromoPrice();
+    public int getDataAllowance();
+    public boolean getUnliCallText();
 }
-
-
-
-
